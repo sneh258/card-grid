d3c7a6b13ecf0da50ee21a597dd4fcd3eb2d4564
Sneha <snehalall.sneha@gmail.com>
2023-07-14 17:20:10 +0530

    feat: create card grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect,useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+function AppOne() {
+  
+    const[post,setPosts]=useState([]);
+
+    const handlePost=async()=>{
+        const response=await axios.get('https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+    }
+
+    useEffect(()=>{
+     handlePost();
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='post-container'>
+        {post.map((post)=>{
+            return <div key={post.id} className='card-container'>
+             <div className='title'><strong>{post.title}</strong></div>
+             <div className='body'>{post.body}</div>
+            </div>
+        })}
     </div>
+    </>
   );
 }
 
-export default App;
+export default AppOne;
